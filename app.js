3e5177d653d8c6428fca8a0939dd46bddfebe333
Edward Stewart <eddstewart1@hotmail.co.uk>
2023-12-02 21:56:41 +0000

    fix visual bug where cursor would show interactivity on hidden videoplayer element

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const watchnow = document.getElementById("watchnow");
+// const watchnow = document.getElementById("watchnow");
 
-function openvideowindow() {
+// function openvideowindow() {
+//     let videowindow = document.getElementById("videowindow");
+//     videowindow.classList.add("show");
+// }
+
+// watchnow.addEventListener("click", openvideowindow);
+
+
+
+const openvideowindow = () => {
     let videowindow = document.getElementById("videowindow");
     videowindow.classList.add("show");
 }
 
 watchnow.addEventListener("click", openvideowindow);
-
-
 
